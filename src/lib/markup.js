fa0b47a7ec4fe06156e4d1dbb77c79797118f595
Harry Llewelyn <harry@hey.com>
2020-09-23 14:17:38 +0100

    Add css loading spinner in.

--- src/lib/markup.js
+++ src/lib/markup.js
@@ -1,8 +1,11 @@
 export default `
 <div class="__PREFIX__box">
   <div class="__PREFIX__content">
-    <div class="__PREFIX__icon"></div>
-    <div class="__PREFIX__text"></div>
+    <div class="spinner">
+      <div class="bounce1"></div>
+      <div class="bounce2"></div>
+      <div class="bounce3"></div>
+    </div>
   </div>
 </div>
 `;
